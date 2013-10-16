7a8b1f5d4aa8ef0034aca89be6578e5b6096d474
S2000Gan <S2k.Peter.Gan@live.com>
2013-10-16 14:23:52 -0700

    Changed Banner Message

--- kern/startup/main.c
+++ kern/startup/main.c
@@ -101,7 +101,7 @@ boot(void)
 	kprintf("%s", harvard_copyright);
 	kprintf("\n");
 
-	kprintf("Put-your-group-name-here's system version %s (%s #%d)\n", 
+	kprintf("Welcome Peter! How are you today? %s (%s #%d)\n", 
 		GROUP_VERSION, buildconfig, buildversion);
 	kprintf("\n");
 
